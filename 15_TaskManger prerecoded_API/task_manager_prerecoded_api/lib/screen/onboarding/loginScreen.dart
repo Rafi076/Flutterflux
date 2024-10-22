34903397433bfeaf877b3b79f99635951865102d
Rafi076 <mohiuddinrafi076@gmail.com>
2024-10-22 23:27:43 +0600

    Login desined

--- 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/onboarding/loginScreen.dart
+++ 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/onboarding/loginScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:task_manager_prerecoded/style/style.dart';
 
 class loginScreen extends StatefulWidget {
   const loginScreen({super.key});
@@ -10,6 +12,34 @@ class loginScreen extends StatefulWidget {
 class _loginScreenState extends State<loginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Get Start With", style: Head1Text(colorDarkBlue),),
+                const SizedBox(height: 1,),
+                Text("Learn With Rafi", style: Head6Text(colorLightGray),),
+                const SizedBox(height: 20,),
+                TextFormField(decoration: AppInputDecoration("Email Address "),),
+                const SizedBox(height: 20,),
+                TextFormField(decoration: AppInputDecoration("Password "),),
+                const SizedBox(height: 20,),
+                Container(child: ElevatedButton(
+                  style: AppButtonStyle(),
+                    onPressed: (){},
+                    child: SuccessButtonChild('Login')),)
+                
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
