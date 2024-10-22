683ba90ac3dd807806bc4cd6ae79bee5e5ddda70
Rafi076 <mohiuddinrafi076@gmail.com>
2024-10-22 23:33:57 +0600

    Registration added

--- 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/onboarding/registrationScreen.dart
+++ 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/onboarding/registrationScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../style/style.dart';
 
 class registrationScreen extends StatefulWidget {
   const registrationScreen({super.key});
@@ -10,6 +13,42 @@ class registrationScreen extends StatefulWidget {
 class _registrationScreenState extends State<registrationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Join With Us", style: Head1Text(colorDarkBlue),),
+                const SizedBox(height: 1,),
+                Text("Learn With Rafi", style: Head6Text(colorLightGray),),
+                const SizedBox(height: 20,),
+                TextFormField(decoration: AppInputDecoration("Email Address "),),
+                const SizedBox(height: 20,),
+                TextFormField(decoration: AppInputDecoration("First Name "),),
+                const SizedBox(height: 20,),
+                TextFormField(decoration: AppInputDecoration("Last Name "),),
+                const SizedBox(height: 20,),
+                TextFormField(decoration: AppInputDecoration("Mobile "),),
+                const SizedBox(height: 20,),
+                TextFormField(decoration: AppInputDecoration("Password "),),
+                const SizedBox(height: 20,),
+                TextFormField(decoration: AppInputDecoration("Confirm Password "),),
+                const SizedBox(height: 20,),
+                Container(child: ElevatedButton(
+                    style: AppButtonStyle(),
+                    onPressed: (){},
+                    child: SuccessButtonChild('Registration')),)
+
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
