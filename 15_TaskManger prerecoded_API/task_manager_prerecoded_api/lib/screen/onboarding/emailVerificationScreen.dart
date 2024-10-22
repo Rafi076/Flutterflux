0ad2e98b0f35569c0827eea89683e494a9445b6b
Rafi076 <mohiuddinrafi076@gmail.com>
2024-10-22 23:43:23 +0600

    EmaiVerified

--- 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/onboarding/emailVerificationScreen.dart
+++ 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/onboarding/emailVerificationScreen.dart
@@ -1,15 +1,60 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../style/style.dart';
 
 class emailVerificationScreen extends StatefulWidget {
   const emailVerificationScreen({super.key});
 
   @override
-  State<emailVerificationScreen> createState() => _emailVerificationScreenState();
+  State<emailVerificationScreen> createState() =>
+      _emailVerificationScreenState();
 }
 
 class _emailVerificationScreenState extends State<emailVerificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Your Email Address",
+                  style: Head1Text(colorDarkBlue),
+                ),
+                const SizedBox(
+                  height: 1,
+                ),
+                Text(
+                  "A 6 digit verification pin will send to your email address",
+                  style: Head6Text(colorLightGray),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: AppInputDecoration("Email "),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  child: ElevatedButton(
+                      style: AppButtonStyle(),
+                      onPressed: () {},
+                      child: SuccessButtonChild('send')),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/onboarding/setPasswordScreen.dart
+++ 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/onboarding/setPasswordScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../style/style.dart';
 
 class setPasswordScreen extends StatefulWidget {
   const setPasswordScreen({super.key});
@@ -10,6 +13,34 @@ class setPasswordScreen extends StatefulWidget {
 class _setPasswordScreenState extends State<setPasswordScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Get Start With", style: Head1Text(colorDarkBlue),),
+                const SizedBox(height: 1,),
+                Text("Learn With Rafi", style: Head6Text(colorLightGray),),
+                const SizedBox(height: 20,),
+                TextFormField(decoration: AppInputDecoration("Email Address "),),
+                const SizedBox(height: 20,),
+                TextFormField(decoration: AppInputDecoration("Password "),),
+                const SizedBox(height: 20,),
+                Container(child: ElevatedButton(
+                    style: AppButtonStyle(),
+                    onPressed: (){},
+                    child: SuccessButtonChild('Login')),)
+
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
