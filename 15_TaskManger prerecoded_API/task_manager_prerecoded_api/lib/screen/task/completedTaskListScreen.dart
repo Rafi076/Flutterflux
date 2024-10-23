3173b36bdebd7cb946236dbcc72a26007b2f2c50
Rafi076 <mohiuddinrafi076@gmail.com>
2024-10-23 23:48:22 +0600

    Complete Task

--- 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/task/completedTaskListScreen.dart
+++ 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/task/completedTaskListScreen.dart
@@ -10,6 +10,6 @@ class completeTaskListScreen extends StatefulWidget {
 class _completeTaskListScreenState extends State<completeTaskListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
