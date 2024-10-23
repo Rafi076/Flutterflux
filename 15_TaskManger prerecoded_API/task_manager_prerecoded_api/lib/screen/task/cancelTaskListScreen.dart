9504aaee78e98f89e588a1a41a315c76c32e5d87
Rafi076 <mohiuddinrafi076@gmail.com>
2024-10-23 23:47:23 +0600

    Cancel Task

---
+++ 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/task/cancelTaskListScreen.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+
+class cancelTasklistScreen extends StatefulWidget {
+  const cancelTasklistScreen({super.key});
+
+  @override
+  State<cancelTasklistScreen> createState() => _cancelTasklistScreenState();
+}
+
+class _cancelTasklistScreenState extends State<cancelTasklistScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/task/completedTaskListScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class completeTaskListScreen extends StatefulWidget {
+  const completeTaskListScreen({super.key});
+
+  @override
+  State<completeTaskListScreen> createState() => _completeTaskListScreenState();
+}
+
+class _completeTaskListScreenState extends State<completeTaskListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
