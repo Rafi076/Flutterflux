9a73cd5d21f499e32995ea2906ee8746dcdce21b
Rafi076 <mohiuddinrafi076@gmail.com>
2024-10-24 00:52:54 +0600

    login Api

--- 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/main.dart
+++ 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Task Manager",
-      initialRoute: '/pinVerification',
+      initialRoute: '/login',
       routes: {
         '/':(context) => splashScreen(),
         '/login':(context) => loginScreen(),
--- 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/onboarding/loginScreen.dart
+++ 15_TaskManger prerecoded_API/task_manager_prerecoded_api/lib/screen/onboarding/loginScreen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager_prerecoded/style/style.dart';
 
+import '../../api/apiClient.dart';
+
 class loginScreen extends StatefulWidget {
   const loginScreen({super.key});
 
@@ -10,6 +12,61 @@ class loginScreen extends StatefulWidget {
 }
 
 class _loginScreenState extends State<loginScreen> {
+
+
+  Map<String, String> FromValues = {
+    // there is only 2 value (email, password) in my POST Login API
+    "email": "",
+    "password": "",
+  };
+  bool Loading = false;
+
+  // to using 2 input field we used 2 time call a {OnPressed} function which can be Expensive. instead of this we will use InputOnChange()
+  InputOnChange(Mapkey, TextValue) {
+    setState(() {
+      FromValues.update(Mapkey, (value) => TextValue);
+    });
+  }
+
+
+
+
+
+  FormOnSubmit() async {
+    if(FromValues['email']!.length == 0){
+      ErrorToast('email Required');
+    }
+    else if(FromValues['password']!.length == 0){
+      ErrorToast('password Required');
+    }
+    else{
+      setState(() {
+        Loading = true;
+      });
+
+      // LoginRequest API Called!
+      bool res=await LoginRequest(FromValues);
+      if(res==true){
+        // Navigate to dashboard page
+        Navigator.pushNamedAndRemoveUntil(context, "/", (route) => false);
+      }
+      else{
+        setState(() {
+          Loading=false;
+        });
+      }
+
+    }
+  }
+
+
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,26 +74,49 @@ class _loginScreenState extends State<loginScreen> {
         children: [
           ScreenBackground(context),
           Container(
-            padding: EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Get Start With", style: Head1Text(colorDarkBlue),),
-                const SizedBox(height: 1,),
-                Text("Learn With Rafi", style: Head6Text(colorLightGray),),
-                const SizedBox(height: 20,),
-                TextFormField(decoration: AppInputDecoration("Email Address "),),
-                const SizedBox(height: 20,),
-                TextFormField(decoration: AppInputDecoration("Password "),),
-                const SizedBox(height: 20,),
-                Container(child: ElevatedButton(
-                  style: AppButtonStyle(),
-                    onPressed: (){},
-                    child: SuccessButtonChild('Login')),)
-                
-              ],
-            ),
+            alignment: Alignment.center,  // to make the content in center
+
+            child: Loading?(Center(child: CircularProgressIndicator(),)):(SingleChildScrollView(
+              // Here -->  child: Loading?(Center(child: CircularProgressIndicator(),)):(SingleChildScrollView( --> this is for showing loading while clicking login if login going to be true
+
+              padding: EdgeInsets.all(30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Get Start With", style: Head1Text(colorDarkBlue),),
+                  const SizedBox(height: 1,),
+                  Text("Learn With Rafi", style: Head6Text(colorLightGray),),
+                  const SizedBox(height: 20,),
+
+                  TextFormField(
+                    decoration: AppInputDecoration("Email Address "),
+                    onChanged: (Textvalue) {
+                      InputOnChange("email", Textvalue);
+                    },
+                  ),
+
+                  const SizedBox(height: 20,),
+
+                  TextFormField(
+                    decoration: AppInputDecoration("Password "),
+                    onChanged: (Textvalue) {
+                      InputOnChange("password", Textvalue);
+                    },
+                  ),
+
+                  const SizedBox(height: 20,),
+
+                  Container(child: ElevatedButton(
+                      style: AppButtonStyle(),
+                      onPressed: (){
+                        FormOnSubmit();
+                      },
+                      child: SuccessButtonChild('Login')),)
+
+                ],
+              ),
+            )),
           )
         ],
       ),
